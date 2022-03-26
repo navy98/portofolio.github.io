0aa5fd853b0b4d82fc6c6bfd20e764500670ee1b
Yosias Navy Muskita <96326245+navy98@users.noreply.github.com>
2022-03-26 16:37:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Progate</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="header-left">
+                <h1>FGA GRUP 27</h1>
+            </div>
+            <!-- Tambahkan ikon menu disini -->
+
+            <div class="header-right">
+                <a href="#">About Us</a>
+                <a href="#">Job Desk</a>
+                <a href="#">Skill</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+    </header>
+    <div class="top-wrapper">
+        <div class="container">
+            <h1>BELAJARAN BERSAMA </h1>
+            <h1>FRONT END DEVELOPER.</h1>
+            <p>Progate adalah platform online untuk belajar coding.</p>
+            <p>Kami menawarkan lingkungan pemograman yang lengkap untuk mempermudah Anda memulai.</p>
+
+        </div>
+    </div>
+    <div class="lesson-wrapper">
+        <div class="container">
+            <div class="heading">
+        
+            </div>
+            <div class="lessons">
+                <div class="lesson">
+                    <div class="lesson-icon">
+                        <img width="200 border-radius: 50px;"  src="1.jpg">
+                        <p>YOSIAS NAVY MUSKITA</p>
+                        <p1 class="text-contents1">Bahasa yang digunakan untuk membuat dan mendesain tampilan situs web Anda. </p1>
+                    </div>
+                    
+                </div>
+                <div class="lesson">
+                    <div class="lesson-icon">
+                        <img src="https://prog-8.com/images/html/advanced/jQuery.png">
+                        <p>YOSIAS NAVY MUSKITA</p>
+                        <p1 class="text-contents1">Bahasa yang digunakan untuk membuat dan mendesain tampilan situs web Anda. </p1>
+                    </div>
+                    
+                </div>
+                <div class="lesson">
+                    <div class="lesson-icon">
+                        <img src="https://prog-8.com/images/html/advanced/ruby.png">
+                        <p>YOSIAS NAVY MUSKITA</p>
+                        <p1 class="text-contents1">Bahasa yang digunakan untuk membuat dan mendesain tampilan situs web Anda. </p1>
+                    </div>
+                    
+                </div>
+                <div class="lesson">
+                    <div class="lesson-icon">
+                        <img src="https://prog-8.com/images/html/advanced/jQuery.png">
+                        <p>YOSIAS NAVY MUSKITA</p>
+                        <p1 class="text-contents1">Bahasa yang digunakan untuk membuat dan mendesain tampilan situs web Anda. </p1>
+                    </div>
+                    
+                </div>
+                <div class="lesson">
+                    <div class="lesson-icon">
+                        <img src="https://prog-8.com/images/html/advanced/ruby.png">
+                        <p>YOSIAS NAVY MUSKITA</p>
+                        <p1 class="text-contents1">Bahasa yang digunakan untuk membuat dan mendesain tampilan situs web Anda. </p1>
+                    </div>
+                    
+                </div>
+            
+                <div class="clear"></div>
+                
+                
+            </div>
+            
+        </div>
+    </div>
+    <div class="message-wrapper">
+        <div class="container">
+            <div class="heading">
+                <h2>Apakah anda siap untuk menjadi programer yang handal?</h2>
+                <h3>Ayo belajar coding, ayo belajar menjadi lebih kreatif!</h3>
+            </div>
+            <span class="btn message">Mulai Belajar</span>
+        </div>
+    </div>
+    <footer>
+        <div class="container">
+            <img src="https://prog-8.com/images/html/advanced/footer_logo.png">
+            <p>Learn to code, learn to be creative.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
